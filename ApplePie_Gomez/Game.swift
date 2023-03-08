c81468a8b7a7212609b60aad204e49b554b50142
Byron Gomez Jr <byrongomezjr@protonmail.com>
2023-03-08 13:43:19 -0500

    updated UI.

---
+++ ApplePie_Gomez/Game.swift
@@ -0,0 +1,15 @@
+//
+//  Game.swift
+//  ApplePie_Gomez
+//
+//  Created by Byron Gomez Jr on 3/8/23.
+//  Copyright © 2023 Byron Gomez. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    
+    var word: String
+    var incorrectMovesRemaining: Int
+}
